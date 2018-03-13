ed35aa6f9b81c2c15e43b416fc1b68f384d86da2
Jcroxford <takeflightsc@gmail.com>
2018-03-13 17:13:03 -0600

    added dynamic rule addition
    rules now spawn a new text field if the last form element is filled out in edit modal

--- public/components/EditModal.vue
+++ public/components/EditModal.vue
@@ -1,7 +1,35 @@
 <template>
   <div :id="modalId" class="modal">
     <div class="modal-content">
+      <!-- editing rules -->
       <h4>Edit Rules</h4>
+      <table>
+        <thead>
+          <tr>
+              <!-- // todo fix widths into classes -->
+              <th style="width: 100px">Points</th>
+              <th>Rule</th>
+              <th style="width: 100px">test</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="(rule, index) of editableRules" :key="rule.id">
+            <td><input type="number" v-model="rule.pts" @blur="handleRuleEdited(rule)" @keyup="checkAddRule(index)"></td>
+            <td>
+              <input type="text" v-model="rule.desc" @blur="handleRuleEdited(rule)" @keyup="checkAddRule(index)">
+            </td>
+            <td><i class="material-icons">delete</i></td>
+          </tr>
+          <!-- <tr> // fixme 
+            <td><input type="text" placeholder="point value" v-model="createdRule.pts"></td>
+            <td><input type="text" placeholder="rule name" v-model="createdRule.desc" @keyup="ruleCreated()"></td>
+          </tr> -->
+        </tbody>
+      </table>
+
+      <!-- editing comments -->
+      <!-- <h4>Edit Comments</h4>
       <table>
         <thead>
           <tr>
@@ -11,18 +39,16 @@
         </thead>
 
         <tbody>
-          <tr v-for="rule of editableRules" :key="rule.id">
+          <tr v-for="comment of editableComments" :key="comment.id">
             <td><input type="number" v-model="rule.pts" @blur="cleanRuleAfterEdit(rule)"></td>
             <td><input type="text" v-model="rule.desc" @blur="cleanRuleAfterEdit(rule)"></td>
           </tr>
           <tr>
-            <td><input type="text"></td>
+            <td><input type="text" placeholder="point value"></td>
+            <td><input type="text" placeholder="rule name"></td>
           </tr>
         </tbody>
-      </table>
-
-      <h4>Edit Comments</h4>
-      <p>A bunch of text</p>
+      </table> -->
     </div>
     <div class="modal-footer">
       <a href="#" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
@@ -40,7 +66,7 @@
     data() {
       return {
         editableRules: [],
-        editableComments: []
+        editableComments: [],
       }
     },
     methods: {
@@ -48,7 +74,7 @@
         return JSON.parse(JSON.stringify(arr))
       },
       // mutates rule in place
-      cleanRuleAfterEdit(rule) {
+      handleRuleEdited(rule) {
         // clean pts
         // todo pts validation is not done yet but in progress
         // const invalidNumVal = /(?!(^-))\D+/g
@@ -58,11 +84,19 @@
         
         // clean rule
         rule.desc = rule.desc.trim().replace(/\s+/g, ' ')
-        console.log(rule) // fixme remove
+      },
+      checkAddRule(ruleIndex) {
+        if(this.editableRules.length - 1 === ruleIndex) this.editableRules.push({ desc: '', pts: null })
+      },
+      ruleCreated() {
+        this.editableRules.push(this.createdRule)
+
+        this.createdRule = { desc: '', pts: null }
       }
     },
     created() {
       this.editableRules = this.deepCopy(this.rules)
+      this.editableRules.push({ desc: '', pts: null })
     }
   }
 </script>
