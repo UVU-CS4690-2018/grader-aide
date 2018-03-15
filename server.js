0dbe5cb9e1ca0b1fd302e5a203e5539dba02b31b
Jcroxford <takeflightsc@gmail.com>
2018-03-15 15:51:56 -0600

    added mongodb connection skeleton

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
-const mongodb = require('mongodb');
+const mongo = require('mongodb').MongoClient;
 const path = require('path');
+require('./config/config')
 
 let app = express();
 
+// db connection
+// todo setup mongodb connection so that we create a new connection every time the db is used and disconnect every time we finishj
+mongo.connect(process.env.MONGODB_URI)
+  .then((client) => console.log('connected to db'))
+  .catch(console.log)
+
+// middleware
 app.use(logger('dev'));
 app.use(helmet());
 app.use(express.static('public'));
 
-let portNum = 3000;
+const port = process.env.PORT || 3001;
 
-app.listen(portNum);
-
-console.log(`Listening on port ${portNum}...\n\n`);
+app.listen(port, () => {
+  console.log(`Listening on port http://localhost:${port}\n\n`);
+});
