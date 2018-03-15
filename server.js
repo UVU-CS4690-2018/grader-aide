2a05e31b4959a140a24b8db02f29fb2edb42ef0d
harryman <harrison.thomas04@gmail.com>
2018-03-15 16:00:13 -0600

    npm dev

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const logger = require('morgan');
 const helmet = require('helmet');
 const mongo = require('mongodb').MongoClient;
 const path = require('path');
-require('./config/config')
+require('./config/config');
 
 let app = express();
 
 // db connection
 // todo setup mongodb connection so that we create a new connection every time the db is used and disconnect every time we finishj
-mongo.connect(process.env.MONGODB_URI)
+mongo
+  .connect(process.env.MONGODB_URI)
   .then((client) => console.log('connected to db'))
-  .catch(console.log)
+  .catch(console.log);
 
 // middleware
 app.use(logger('dev'));
@@ -21,5 +22,5 @@ app.use(express.static('public'));
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Listening on port http://localhost:${port}\n\n`);
+  console.log(`Listening on http://localhost:${port}\n`);
 });
